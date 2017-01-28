796a9a603a621cfac96a12c11da62dee04a15672
EWHS-4077 <EWHS-4077@EWHSrobotics>
2017-01-27 17:03:07 -0800

    Tank to Arcade

--- src/org/usfirst/frc/team4077/robot/Robot.java
+++ src/org/usfirst/frc/team4077/robot/Robot.java
@@ -1,12 +1,10 @@
 package org.usfirst.frc.team4077.robot;
 
 import com.ctre.CANTalon;
-
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
@@ -18,14 +16,16 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	
 	CANTalon frontLeft = new CANTalon(3);
 	CANTalon rearLeft = new CANTalon(4);
 	CANTalon frontRight = new CANTalon(1);
 	CANTalon rearRight= new CANTalon(2);
-	Joystick left, right; 
+	Joystick stick = new Joystick(0);
 	Timer timer = new Timer();
 	RobotDrive myRobot = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
+
+	    
+	
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -33,9 +33,10 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
-		left = new Joystick(1);
-		right = new Joystick(0);
+
 		CameraServer.getInstance().startAutomaticCapture();
+		
+	
 	}
 
 	/**
@@ -56,20 +57,22 @@ public class Robot extends IterativeRobot {
 		if (timer.get() < 2.0) {
 			myRobot.drive(-0.5, 0.0); // drive forwards half speed
 		} else if (timer.get() <3.0) {
-			myRobot.tankDrive(0.73, -0.73);
+			myRobot.tankDrive(0.71, -0.71);
 			
 		} else if (timer.get() <5.0) {
 			myRobot.drive(-0.5, 0.0);
 		} else if (timer.get() <6.0){
-		myRobot.tankDrive(0.73, -0.73);
+		myRobot.tankDrive(0.71, -0.71);
 		
 		} else if (timer.get() <8.0) {
 			myRobot.drive(-0.5, 0.0);
 		} else if (timer.get() <9.0) {
-			myRobot.tankDrive(0.73, -0.73);
+			myRobot.tankDrive(0.71, -0.71);
 			
 		} else if (timer.get() <11.0) {
 			myRobot.drive(-0.5, 0.0);
+		} else if (timer.get() <12.0) {
+			myRobot.tankDrive(0.71, -0.71);
 		} else {	
 			myRobot.drive(0.0, 0.0); // stop robot
 		}
@@ -88,7 +91,9 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
-		myRobot.tankDrive(left, right);
+		myRobot.arcadeDrive(stick);
+		System.out.println("x-axis; " + stick.getX());
+		System.out.println("y-axis; " + stick.getX());
 
 	}
 
